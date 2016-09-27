2bdc50fcf2ae3b2bb74b68929941680e445ced3c
exmelenez <exmelendez@gmail.com>
2016-09-26 22:05:22 -0400

    init commit

---
+++ app/src/main/java/nyc/c4q/eddie/simongame/MainActivity.java
@@ -0,0 +1,44 @@
+package nyc.c4q.eddie.simongame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView scoreBoard = (TextView)findViewById(R.id.score_board);
+        final Button blueBt = (Button) findViewById(R.id.blueBt);
+
+        blueBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                blueBt.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                return false;
+            }
+        });
+
+
+
+        blueBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+    }
+
+    public void buttonClicked (View v){
+//        v.setBackgroundColor();
+
+
+
+    }
+}
